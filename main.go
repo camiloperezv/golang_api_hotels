772e016d882fa9cf813c33401f8e3c8ceef9be75
Camilo Posada Angel <cposadaa@gmail.com>
2017-11-23 16:06:01 +0000

    Firebase sdk, autenticación por token

--- main.go
+++ main.go
@@ -153,54 +153,6 @@ func ValidateMiddleware(next http.HandlerFunc) http.HandlerFunc {
 		}
 	})
 
-	// return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-	// 	authorizationHeader := req.Header.Get("authorization")
-	// 	if authorizationHeader != "" {
-	// 		bearerToken := strings.Split(authorizationHeader, " ")
-	// 		if len(bearerToken) == 2 {
-
-	// 			// auth, _ := firebase.GetAuth()
-	// 			// decodedToken, err := auth.VerifyIDToken(bearerToken[1])
-	// 			// if err != nil {
-	// 			// 	json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
-	// 			// }
-
-	// 			// uid, found := decodedToken.UID()
-	// 			// context.Set(req, "decoded", uid)
-	// 			// context.Set(req, "FOUND", found)
-	// 			// next(w, req)
-
-	// 			// println("decoded", decodedToken)
-
-	// 			// auth, _ := firebase.GetAuth()
-	// 			// decodedToken, err := auth.VerifyIDToken(idTokenString)
-	// 			// if err == nil {
-	// 			// 	uid, found := decodedToken.Uid()
-	// 			// 	next(w, req)
-	// 			// }
-
-	// 			// token, error := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
-	// 			// 	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-	// 			// 		return nil, fmt.Errorf("There was an error")
-	// 			// 	}
-	// 			// 	return []byte(""), nil
-	// 			// })
-	// 			// println(token.Claims)
-	// 			// if error != nil {
-	// 			// 	json.NewEncoder(w).Encode(Exception{Message: error.Error()})
-	// 			// 	return
-	// 			// }
-	// 			// if token.Valid {
-	// 			// 	context.Set(req, "decoded", token.Claims)
-	// 			// 	next(w, req)
-	// 			// } else {
-	// 			// 	json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
-	// 			// }
-	// 		}
-	// 	} else {
-	// 		json.NewEncoder(w).Encode(Exception{Message: "An authorization header is required"})
-	// 	}
-	// })
 }
 
 func TestEndpoint(w http.ResponseWriter, req *http.Request) {
