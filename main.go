587ef7b19c3278c084ad25a6f3488ac3ab67752b
Camilo Posada Angel <cposadaa@gmail.com>
2017-11-24 22:28:09 +0000

    id de usuario se guarda en la reserva

--- main.go
+++ main.go
@@ -326,6 +326,8 @@ func getRoomsAvailable(w http.ResponseWriter, r *http.Request) {
 func getReservationRequest(w http.ResponseWriter, r *http.Request) {
 
 	// establecer conexión con Base de Datos
+
+	userId := context.Get(r, "uid")
 	session, err := mgo.Dial("mongodb://udeain:udeainmongodb@ds157444.mlab.com:57444/heroku_4r2js6cs")
 	if err != nil {
 		panic(err)
@@ -482,11 +484,9 @@ func getReservationRequest(w http.ResponseWriter, r *http.Request) {
 
 	// guardar datos de reserva
 	id_reserva := bson.NewObjectId().Hex()
-	collection.Insert(bson.M{"reserve_id": id_reserva, "_id": id_reserva, "arrive_date": arrive_date, "leave_date": leave_date, "state": "A", "host_id": "0045123", "hotel_id": hotel_id,
-		/*"beds_double": beds_double, "beds_simple": beds_simple,*/ "doc_type": doc_type, "doc_id": doc_id,
-		"email": email, "phone_number": phone_number, "room_id": room_id,
-		"room": bson.M{"room_thumbnail": room_thumbnail, "room_price": room_price, "description": room_description, "currency": "COP", "room_type": room_type, "capacity": capacity_number, "beds": bson.M{"simple": beds_simple, "double": beds_double}},
-	})
+	collection.Insert(bson.M{"userId": userId, "_id": id_reserva, "start_date": arrive_date, "end_date": leave_date, "state": "awaiting", "host_id": "0045123", "hotel_id": hotel_id,
+		"room_type": room_type, "capacity": capacity_number, "beds_double": beds_double, "beds_simple": beds_simple, "doc_type": doc_type, "doc_id": doc_id,
+		"email": email, "phone_number": phone_number, "room_id": room_id})
 	println("ID reserva generada: " + id_reserva)
 
 	// retornar respuesta de reserva gestionada
@@ -513,7 +513,7 @@ func main() {
 	//r.HandleFunc("/api/v1/rooms/arrive_date/{arriveDate}/leave_date/{leaveDate}/city/{city}/hosts/{hosts}/room_type/{roomType}", getRooms).Methods("GET")
 	r.HandleFunc("/api/v1/rooms", getRooms).Methods("GET")
 	r.HandleFunc("/api/v1/rooms_info", getRoomsAvailable).Methods("GET")
-	r.HandleFunc("/api/v1/rooms/reserve", getReservationRequest).Methods("POST")
+	r.HandleFunc("/api/v1/rooms/reserve", ValidateMiddleware(getReservationRequest)).Methods("POST")
 
 	r.HandleFunc("/api/v1/reservations", getReservations).Methods("GET")
 	r.HandleFunc("/test", ValidateMiddleware(TestEndpoint)).Methods("GET")
